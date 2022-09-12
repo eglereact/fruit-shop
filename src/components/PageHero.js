45020d14242785bbc818f5ad2434b34e043114e0
eglereact <eglereactjs@gmail.com>
2022-09-12 13:00:28 +0300

    basic product info

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
 
-function PageHero({ title, image }) {
+function PageHero({ title, image, product }) {
   return (
     <div className="relative w-full h-40 overflow-hidden my-6">
       <img src={image} alt="Avatar" className="object-cover w-full h-full" />
       <div
         className="absolute w-full h-full bottom-0 flex justify-center items-center inset-x-0 bg-gray-800/30
-        text-white text-xs text-center leading-4"
+        text-white text-xs text-center leading-4 flex-col"
       >
-        <h3 className="text-4xl">
+        <h2 className="text-4xl font-bold mb-2 capitalize">{title}</h2>
+        <h3 className="text-2xl">
           <Link to="/" className="hover:text-gray-800">
             Home
           </Link>{" "}
-          / <span className="font-bold">{title}</span>
+          /{" "}
+          {product && (
+            <>
+              <Link to="/shop" className="hover:text-gray-800">
+                Shop
+              </Link>
+              <span> / </span>
+            </>
+          )}
+          <span className="font-bold capitalize">{title}</span>
         </h3>
       </div>
     </div>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useProductsContext } from "../context/product_context";
+import PageHero from "../components/PageHero";
+import { socialIcons } from "../data.js";
 
 function SingleProduct() {
   const { id } = useParams();
@@ -34,7 +36,71 @@ function SingleProduct() {
   }
 
   console.log(product);
-  return <div>SingleProduct {product?.fields?.name}</div>;
+
+  const {
+    name,
+    rating,
+    price,
+    description,
+    stock,
+    id: sku,
+    image,
+    category,
+    shipping,
+    weight,
+    color,
+    available,
+  } = product?.fields || {};
+
+  const { url } = image?.[0] || "";
+
+  console.log(url);
+
+  return (
+    <main>
+      <PageHero title={name} image={url} product />
+      <div className="max-w-6xl mx-auto">
+        <div className="grid md:grid-cols-2 mt-16 mx-4">
+          <section>Images</section>
+          <section>
+            <h2 className="text-gray-800 text-4xl font-bold pb-2 capitalize">
+              {name}
+            </h2>
+            <div>★★★★★</div>
+            <h5 className="text-red-500 text-3xl py-4">${price}</h5>
+            <p className="text-gray-500 mb-10">{description}</p>
+            <div>Cart</div>
+            <div className="border-b border-gray-200 my-10"></div>
+            <div className="flex flex-col w-full text-gray-800">
+              <p className="flex">
+                <span className="product-info">Availability</span>{" "}
+                {available ? "In Stock" : "Out of Stock"}
+              </p>
+              <p className="flex">
+                <span className="product-info">Shipping</span> 01 day shipping.
+                <span className="text-red-500 pl-1">Free pickup today</span>
+              </p>
+              <p className="flex">
+                <span className="product-info">Weight</span> {weight} kg
+              </p>
+              <p className="flex items-center">
+                <span className="product-info">Share on</span>
+                {socialIcons.map((item) => (
+                  <a
+                    key={item.id}
+                    href={item.link}
+                    className="hover:text-[#7fad39] mr-4"
+                  >
+                    {item.icon}
+                  </a>
+                ))}
+              </p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default SingleProduct;
