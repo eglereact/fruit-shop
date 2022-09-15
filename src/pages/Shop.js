1ceeaba2b0deb83bad709572b2ad5ee09330c6cd
eglereact <eglereactjs@gmail.com>
2022-09-15 15:38:57 +0300

    filter context setup

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,14 +2,15 @@ import PageHero from "../components/PageHero";
 
 function Shop() {
   return (
-    <div className="max-w-6xl mx-auto">
-      <div className="mx-4">
-        <PageHero
-          title="Shop"
-          image="https://wallpaperaccess.com/full/2847339.jpg"
-        />
+    <>
+      <PageHero
+        title="Shop"
+        image="https://wallpaperaccess.com/full/2847339.jpg"
+      />
+      <div className="max-w-6xl mx-auto">
+        <div className="mx-4">Store</div>
       </div>
-    </div>
+    </>
   );
 }
 
